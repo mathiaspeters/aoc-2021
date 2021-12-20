0e44c152bd49f445c82ef9d7edd15f7cc6fbd4e5
Mathias Peters <mathias.peters6@gmail.com>
2021-12-20 16:38:47 +0100

    Day 19 cleanup, still WIP

--- src/day19/mod.rs
+++ src/day19/mod.rs
@@ -5,47 +5,34 @@ pub fn day19() {
 
 pub fn part1() -> usize {
     let mut scanners = input();
-    let mut common: Vec<Vec<(usize, Vec<(usize, usize)>)>> = vec![];
-    for i in 0..scanners.len() {
-        let mut com = vec![];
-        for j in i + 1..scanners.len() {
-            if let Some(c) = check_common(&scanners[i], &scanners[j]) {
-                com.push((j, c));
-            }
+    let common = get_common(&scanners);
+    common.iter().for_each(|((i, j), c)| {
+        println!("\nCommon {}-{}", i, j);
+        c.iter().for_each(|(a, b)| println!("{},{}", a, b));
+    });
+    common.iter().for_each(|((i, j), c)| {
+        let i = *i;
+        let j = *j;
+        println!("\nFinding common beacons for: {}, {}", i, j);
+        let mut s2 = scanners[j].clone();
+        println!("\nScanner {}", i);
+        for index in 0..scanners[i].readings[0].len() {
+            println!(
+                "{},{},{}",
+                scanners[i].readings[0][index],
+                scanners[i].readings[1][index],
+                scanners[i].readings[2][index]
+            );
         }
-        common.push(com);
-    }
-    dbg!(&common);
-    common.iter().enumerate().for_each(|(i, c)| {
-        c.iter().for_each(|(j, c2)| {
-            let j = if i == 2 { 2 } else { *j };
-            let i = if i == 2 { 4 } else { i };
-            let mapped_c2 = c2.iter().map(|(a, b)| (*b, *a)).collect::<Vec<_>>();
-            let c2 = if j == 2 { &mapped_c2 } else { c2 };
-
-            //println!("{}, {}", i, j);
-            let mut s2 = scanners[j].clone();
-            /*println!("\nCommon {}-{}", i, j);
-            c2.iter().for_each(|(a, b)| println!("{},{}", a, b));
-            println!("\nScanner {}", i);
-            for index in 0..scanners[i].readings[0].len() {
-                println!(
-                    "{},{},{}",
-                    scanners[i].readings[0][index],
-                    scanners[i].readings[1][index],
-                    scanners[i].readings[2][index]
-                );
-            }
-            println!("\nScanner {}", j);
-            for index in 0..scanners[0].readings[0].len() {
-                println!(
-                    "{},{},{}",
-                    s2.readings[0][index], s2.readings[1][index], s2.readings[2][index]
-                );
-            }*/
-            calculate_offset(&scanners[i], &mut s2, &c2);
-            std::mem::swap(&mut scanners[j], &mut s2);
-        });
+        println!("\nScanner {}", j);
+        for index in 0..scanners[0].readings[0].len() {
+            println!(
+                "{},{},{}",
+                s2.readings[0][index], s2.readings[1][index], s2.readings[2][index]
+            );
+        }
+        calculate_offset(&scanners[i], &mut s2, &c);
+        std::mem::swap(&mut scanners[j], &mut s2);
     });
     let mut beacons: Vec<(i16, i16, i16)> = vec![];
     scanners.iter().for_each(|s| {
@@ -65,76 +52,72 @@ pub fn part2() -> usize {
 }
 
 fn calculate_offset(s1: &Scanner, s2: &mut Scanner, common: &[(usize, usize)]) {
-    let (m1, m2, m3) = if check_axis(&s1.readings[0], &s2.readings[0], common) {
-        println!("x");
-        (0, 1, 2)
-    } else if check_axis(&s1.readings[0], &s2.readings[1], common) {
-        println!("y");
-        (1, 2, 0)
-    } else {
-        println!("z");
-        (2, 0, 1)
-    };
-    let x_rev = common.iter().fold(true, |acc, (a, b)| {
-        acc && s1.readings[0][*a].signum() != s2.readings[m1][*b].signum()
-    });
-    let y_rev = common.iter().fold(true, |acc, (a, b)| {
-        acc && s1.readings[1][*a].signum() != s2.readings[m2][*b].signum()
-    });
-    let z_rev = common.iter().fold(true, |acc, (a, b)| {
-        acc && s1.readings[2][*a].signum() != s2.readings[m3][*b].signum()
-    });
-    dbg!(
-        x_rev,
-        s1.readings[0][common[0].0],
-        s2.readings[m1][common[0].1],
-        y_rev,
-        s1.readings[1][common[0].0],
-        s2.readings[m2][common[0].1],
-        z_rev,
-        s1.readings[2][common[0].0],
-        s2.readings[m3][common[0].1],
-    );
-    let mut x = s1.readings[0][common[0].0];
-    let mut y = s1.readings[1][common[0].0];
-    let mut z = s1.readings[2][common[0].0];
-    if x_rev {
-        x += s2.readings[m1][common[0].1];
-    } else {
-        x -= s2.readings[m1][common[0].1];
-    }
-    if y_rev {
-        y -= s2.readings[m2][common[0].1];
-    } else {
-        y += s2.readings[m2][common[0].1];
-    }
-    if z_rev && y_rev == x_rev {
-        z += s2.readings[m3][common[0].1];
-    } else {
-        z -= s2.readings[m3][common[0].1];
-    }
-    println!("({},{},{})", x, y, z);
+    let (x_mul, x) = (0..3)
+        .find(|i| check_axis(&s1.readings[0], &s2.readings[*i], common))
+        .map(|i| get_diff(&s1.readings[0], &s2.readings[i], common))
+        .unwrap();
+    let (y_mul, y) = (0..3)
+        .find(|i| check_axis(&s1.readings[1], &s2.readings[*i], common))
+        .map(|i| get_diff(&s1.readings[1], &s2.readings[i], common))
+        .unwrap();
+    let (z_mul, z) = (0..3)
+        .find(|i| check_axis(&s1.readings[2], &s2.readings[*i], common))
+        .map(|i| get_diff(&s1.readings[2], &s2.readings[i], common))
+        .unwrap();
+    println!("({},{}),({},{}),({},{})", x, x_mul, y, y_mul, z, z_mul);
 
-    let xm = if x_rev { -1 } else { 1 };
-    let zm = if z_rev { -1 } else { 1 };
     for i in 0..s2.readings[0].len() {
-        let mut new_x = s2.readings[m1][i] + x * xm;
-        if x_rev {
-            new_x *= xm;
-        }
-        let new_y = s2.readings[m2][i] + y;
-        let mut new_z = s2.readings[m3][i] + z * zm;
-        if z_rev {
-            new_z *= zm;
-        }
-        s2.readings[0][i] = new_x;
-        s2.readings[1][i] = new_y;
-        s2.readings[2][i] = new_z;
+        s2.readings[0][i] -= x;
+        s2.readings[0][i] *= x_mul;
+        s2.readings[1][i] -= y;
+        s2.readings[1][i] *= y_mul;
+        s2.readings[2][i] -= z;
+        s2.readings[2][i] *= z_mul;
     }
+}
+
+fn get_diff(r: &[i16], a: &[i16], m: &[(usize, usize)]) -> (i16, i16) {
+    let ref1 = a[m[0].1] - r[m[0].0];
+    let ref2 = a[m[0].1] + r[m[0].0];
+    for i in 1..m.len() {
+        let cmp1 = a[m[i].1] - r[m[i].0];
+        let cmp2 = a[m[i].1] + r[m[i].0];
+        if cmp1 == ref1 && cmp2 == ref2 {
+            continue;
+        } else if cmp1 == ref1 {
+            return (1, ref1);
+        } else {
+            return (-1, cmp2);
+        }
+    }
+    panic!("All of them are the same...");
 }
 
 fn check_axis(r: &[i16], a: &[i16], m: &[(usize, usize)]) -> bool {
     (r[m[0].0] - r[m[1].0]).abs() == (a[m[0].1] - a[m[1].1]).abs()
+}
+
+fn get_common(scanners: &[Scanner]) -> Vec<((usize, usize), Vec<(usize, usize)>)> {
+    let mut output = vec![];
+    let mut remaining = (1..scanners.len()).collect::<Vec<_>>();
+    let mut open = vec![0];
+    let mut closed = vec![];
+    while !remaining.is_empty() {
+        let current = open.remove(0);
+        let mut to_remove = vec![];
+        remaining.iter().enumerate().for_each(|(index, r)| {
+            if let Some(c) = check_common(&scanners[current], &scanners[*r]) {
+                to_remove.push(index);
+                open.push(*r);
+                output.push(((current, *r), c));
+            }
+        });
+        to_remove.into_iter().rev().for_each(|i| {
+            remaining.remove(i);
+        });
+        closed.push(current);
+    }
+    output
 }
 
 fn check_common(s1: &Scanner, s2: &Scanner) -> Option<Vec<(usize, usize)>> {
@@ -155,7 +138,6 @@ fn check_permutations(p1: &[[i16; 3]], p2: &[[i16; 3]]) -> Vec<(usize, usize)> {
         for j in 0..p2.len() {
             if v1.iter().all(|v3| p2[j].contains(v3)) {
                 common.push((i, j));
-                break;
             }
         }
     });
